067745c4cc7876b6a38fcff9886b4f286ae0be47
vyonna <vyonna.njenga@student.moringaschool.com>
2022-02-06 09:41:22 +0300

    added myform component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { MyformComponent } from './myform/myform.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    QuotesComponent,
+    MyformComponent
   ],
   imports: [
     BrowserModule,
